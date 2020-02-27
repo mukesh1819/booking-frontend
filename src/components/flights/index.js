f5797a080af29bdf159bfa3f56e933be6a4a9a4a
ahnupsingh <anup.singh2071@gmail.com>
2020-02-27 15:15:48 +0545

    Fixed tab issue in home page

--- src/components/flights/index.js
+++ src/components/flights/index.js
@@ -6,31 +6,42 @@ import './flights.scss';
 import TabView from '../shared/TabView';
 import Sidebar from '../shared/Sidebar';
 import Banner from '../shared/Banner';
+import {Tabs, Tab} from 'react-bootstrap';
+
 export default class Flights extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			key: 'flights'
+		};
+	}
+
+	setKey(key) {
+		this.setState({
+			key: key
+		});
+	}
+
 	render() {
+		const {key} = this.state;
 		const tabs = [{name: 'flights', label: 'Flights'}, {name: 'packages', label: 'Packages'}];
 		return (
 			<React.Fragment>
-				<TabView tabs={tabs}>
-					<div
-						class='tab-pane fade active show bg-body'
-						id='nav-flights'
-						role='tabpanel'
-						aria-labelledby='nav-flights-tab'
-					>
+				<Tabs id='home-tab' activeKey={key} className='dnav-fill' onSelect={(k) => this.setKey(k)}>
+					<Tab eventKey='flights' title='Flights'>
 						<SearchFlightForm />
 						<div className='container text-center'>
 							<h4> Things to do </h4>
 							{/* <PackageList /> */}
 						</div>
-					</div>
-					<div class='tab-pane fade' id='nav-packages' role='tabpanel' aria-labelledby='nav-packages-tab'>
+					</Tab>
+					<Tab eventKey='packages' title='Packages'>
 						<div className='container'>
 							<h4> Popular Packages </h4>
 							<PackageList />
 						</div>
-					</div>
-				</TabView>
+					</Tab>
+				</Tabs>
 				<Sidebar />
 			</React.Fragment>
 		);
