7158b23220e73ad02c61b1670478919e760d09a8
ahnupsingh <anup.singh2071@gmail.com>
2020-04-08 10:44:47 +0545

    Fix download ticket in payment success page

--- src/components/payments/PaymentSuccess.js
+++ src/components/payments/PaymentSuccess.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {newPayment} from '../../api/paymentApi';
 import swal from 'sweetalert';
 import successImage from '../../images/success.png';
+import {downloadTicket} from '../../api/flightApi';
 
 class PaymentSuccess extends Component {
 	constructor(props) {
@@ -13,17 +14,21 @@ class PaymentSuccess extends Component {
 	componentDidUpdate() {}
 
 	render() {
-		const {idx} = this.props.match.params.idx;
+		const {idx} = this.props.match.params;
 		return (
 			<div className='container'>
 				<div className='card'>
 					<div className='card-body text-center'>
 						<h3 className='title'>Payment Successfull</h3>
-						<span>
-							<img src={successImage} />
-						</span>
+						<div className='text-center'>
+							<img src={successImage} style={{width: '100px', height: '100px'}} />
+						</div>
 
-						<div className='btn btn-secondary'>Download Ticket</div>
+						<div>
+							<span onClick={() => downloadTicket(idx)} className='btn btn-secondary btn-large'>
+								Download ticket
+							</span>
+						</div>
 					</div>
 				</div>
 			</div>
