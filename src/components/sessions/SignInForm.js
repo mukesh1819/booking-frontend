671e9d810d980b3ebd39e62351fc9d5aa5d96b42
ahnupsingh <anup.singh2071@gmail.com>
2020-04-30 15:32:34 +0545

    Change secondary color

--- src/components/sessions/SignInForm.js
+++ src/components/sessions/SignInForm.js
@@ -35,123 +35,119 @@ class SignInForm extends Component {
 		// 	return <Redirect to={redirectUrl} />;
 		// }
 		return (
-			<div className='container login-page'>
-				<div className='row justify-content-center align-items-center'>
-					<div className='col-12 col-md-4 p-0'>
-						<Formik
-							initialValues={{
-								email: '',
-								password: ''
-							}}
-							onSubmit={async (values, {setSubmitting, setStatus}) => {
+			<div className='container full-page'>
+				<Formik
+					initialValues={{
+						email: '',
+						password: ''
+					}}
+					onSubmit={async (values, {setSubmitting, setStatus}) => {
+						this.setState({
+							loading: true
+						});
+						const variables = {
+							user: {
+								email: values.email,
+								password: values.password
+							}
+						};
+						signIn(variables)
+							.then((response) => {
+								setSubmitting(false);
+								if (response.data.user !== undefined) {
+									// console.log('Logged In user', response);
+									this.props.loginUser(response.data.user);
+									localStorage.setItem('token', response.data.jwt);
+									var path = roleBasedUrl(response.data.user.role, redirectUrl);
+									history.push(path);
+								} else {
+									console.log('sign in failed error');
+								}
 								this.setState({
-									loading: true
+									loading: false
 								});
-								const variables = {
-									user: {
-										email: values.email,
-										password: values.password
-									}
-								};
-								signIn(variables)
-									.then((response) => {
-										setSubmitting(false);
-										if (response.data.user !== undefined) {
-											// console.log('Logged In user', response);
-											this.props.loginUser(response.data.user);
-											localStorage.setItem('token', response.data.jwt);
-											var path = roleBasedUrl(response.data.user.role, redirectUrl);
-											history.push(path);
-										} else {
-											console.log('sign in failed error');
-										}
-										this.setState({
-											loading: false
-										});
-									})
-									.catch((error) => {
-										console.log('SIGN IN failed error', error);
-										this.setState({
-											loading: false
-										});
-									});
-							}}
-						>
-							{({
-								values,
-								errors,
-								touched,
-								handleChange,
-								handleBlur,
-								handleSubmit,
-								isSubmitting,
-								setFieldValue
-								/* and other goodies */
-							}) => (
-								<Segment loading={loading}>
-									<div className='login-form card p-2'>
-										<div className='card-body'>
-											<h3>Log in</h3>
-											<div className='text-small'>
-												Don't have an account?{' '}
-												<Link to={{pathname: '/signup', state: {from: redirectUrl}}}>
-													Create Account
-												</Link>
-											</div>
-											<form onSubmit={handleSubmit} className='form-wrap'>
-												<div className='fields'>
-													<div className='field'>
-														<label>Email</label>
-
-														<Field
-															type='text'
-															name='email'
-															className='form-control'
-															onBlur={handleBlur}
-															onChange={handleChange}
-															value={values.email}
-															placeholder='Email'
-														/>
-													</div>
-
-													<div className='field'>
-														<label>Password</label>
-
-														<Field
-															type='password'
-															name='password'
-															className='form-control'
-															onBlur={handleBlur}
-															onChange={handleChange}
-															value={values.password}
-															placeholder='Password'
-														/>
-													</div>
-													<div className='text-small'>
-														<Link to='/forgot'>Forgot Password?</Link>
-													</div>
-												</div>
-												<button
-													className='btn btn-secondary my-2 w-100'
-													type='submit'
-													disabled={isSubmitting}
-												>
-													Sign In
-												</button>
-												<hr />
-												<div className='text-center text-small mb-2'>Sign In with </div>
-												<SocialLinks
-													redirectUrl={redirectUrl}
-													setLoading={(status) => this.setLoading(status)}
-												/>
-											</form>
-										</div>
+							})
+							.catch((error) => {
+								console.log('SIGN IN failed error', error);
+								this.setState({
+									loading: false
+								});
+							});
+					}}
+				>
+					{({
+						values,
+						errors,
+						touched,
+						handleChange,
+						handleBlur,
+						handleSubmit,
+						isSubmitting,
+						setFieldValue
+						/* and other goodies */
+					}) => (
+						<Segment loading={loading}>
+							<div className='login-form card p-2'>
+								<div className='card-body'>
+									<h3>Log in</h3>
+									<div className='text-small'>
+										Don't have an account?{' '}
+										<Link to={{pathname: '/signup', state: {from: redirectUrl}}}>
+											Create Account
+										</Link>
 									</div>
-								</Segment>
-							)}
-						</Formik>
-					</div>
-				</div>
+									<form onSubmit={handleSubmit} className='form-wrap'>
+										<div className='fields'>
+											<div className='field'>
+												<label>Email</label>
+
+												<Field
+													type='text'
+													name='email'
+													className='form-control'
+													onBlur={handleBlur}
+													onChange={handleChange}
+													value={values.email}
+													placeholder='Email'
+												/>
+											</div>
+
+											<div className='field'>
+												<label>Password</label>
+
+												<Field
+													type='password'
+													name='password'
+													className='form-control'
+													onBlur={handleBlur}
+													onChange={handleChange}
+													value={values.password}
+													placeholder='Password'
+												/>
+											</div>
+											<div className='text-small'>
+												<Link to='/forgot'>Forgot Password?</Link>
+											</div>
+										</div>
+										<button
+											className='btn btn-secondary my-2 w-100'
+											type='submit'
+											disabled={isSubmitting}
+										>
+											Sign In
+										</button>
+										<hr />
+										<div className='text-center text-small mb-2'>Sign In with </div>
+										<SocialLinks
+											redirectUrl={redirectUrl}
+											setLoading={(status) => this.setLoading(status)}
+										/>
+									</form>
+								</div>
+							</div>
+						</Segment>
+					)}
+				</Formik>
 			</div>
 		);
 	}
