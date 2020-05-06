57d0be061f09f3efeeb86eb5837583a3fa40b6cb
ahnupsingh <anup.singh2071@gmail.com>
2020-05-06 18:46:33 +0545

    .

--- src/helpers/validations.js
+++ src/helpers/validations.js
@@ -6,23 +6,23 @@ export function phoneValidate(yup) {
         .length(10, 'Number length should be exactly 10');
 }
 
-export function textValidate(yup){
+export function textValidate(yup) {
     return yup
         .string()
         .typeError('Not a valid text')
         .matches(/^[A-Za-z ]+$/, 'Please enter a valid text');
 }
 
-export function alphaNumericValidate(yup){
+export function alphaNumericValidate(yup) {
     return yup
         .string()
         .typeError('Not a valid number pr text')
         .matches(/^[ A-Za-z0-9_@.]*$/, 'Please enter a valid text or number');
 }
 
-export function numberValidate(yup){
+export function numberValidate(yup) {
     return yup
         .string()
-        .typeError('Not a valid text')
-        .matches(new RegExp("^\\d+$"), 'Please enter a valid text');
+        .typeError('Not a valid number')
+        .matches(new RegExp("^\\d+$"), 'Please enter a valid number');
 }
